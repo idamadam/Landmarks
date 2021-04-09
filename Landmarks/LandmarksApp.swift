dddc8ca0a2481c5e007c31aa860668e1422757e7
Idam Adam <contact@idamadam.com>
2021-04-09 14:40:39 +1000

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Idam Adam on 9/4/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
